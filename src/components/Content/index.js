f0c9c7736498e6cc9e7e4c7e30ac73bf09675ec4
Eduard Shavyrko <edshav@protonmail.com>
2019-07-05 10:40:41 +0300

    replace index.html

---
+++ src/components/Content/index.js
@@ -0,0 +1,22 @@
+import './style.scss';
+
+import React from 'react';
+// import UserView from '../UserView';
+// import Features from '../Features';
+// import UserMainInfo from '../UserMainInfo';
+// import UserDetails from '../UserDetails';
+// import UserContacts from '../UserContacts';
+
+const Content = () => {
+  return (
+    <div className="Content">
+      {/* <UserView />
+      <Features />
+      <UserContacts />
+      <UserMainInfo />
+      <UserDetails /> */}
+    </div>
+  );
+};
+
+export default Content;
